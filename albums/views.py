096f5c79b92bb8295b3d280e601cab6517b5a503
Rachel Kelly <racheldk126@gmail.com>
2022-06-30 16:02:07 -0700

    Added 'Add Album' button, form. data added to list_albums page when submitted

--- albums/views.py
+++ albums/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render, redirect, get_list_or_404
+
+from .forms import AlbumForm
 from .models import Album
 
 
 def list_albums(request):
     albums = Album.objects.all()
     return render(request, "albums/list_albums.html", {"albums": albums})
+
+
+def new_album(request):
+    if request.method == 'GET':
+        form = AlbumForm()
+    else:
+        form = AlbumForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(to='list_albums')   
+
+    return render(request, "albums/new_album.html", {'form':form})             
--- django_music/urls.py
+++ django_music/urls.py
@@ -22,6 +22,7 @@ from albums import views as albums_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', albums_views.list_albums, name='list_albums'),
+    path('/albums/new/', albums_views.new_album, name='new_album'), 
 ]
 
 
