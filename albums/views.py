dd45aa86a3afaee319b6784f143c2caa523b541c
Rachel Kelly <racheldk126@gmail.com>
2022-07-06 10:55:53 -0700

    Artist showing on browser page list_albums

--- albums/views.py
+++ albums/views.py
@@ -7,9 +7,8 @@ from .models import Album, Artist, Favorite
 def list_albums(request):
     albums = Album.objects.all()
     favorite_albums = [album for album in albums if album.check_is_user_favorite(request.user)]
-    artists = Artist.objects.all()
         
-    return render(request, "albums/list_albums.html", {"albums": albums, "favorite_albums": favorite_albums, "artists": artists})
+    return render(request, "albums/list_albums.html", {"albums": albums, "favorite_albums": favorite_albums})
 
 
 def new_album(request):
